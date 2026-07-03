30dbabb7412d122db45753d5cc59b5a838475ce6
WittmannD <danjavitman@gmail.com>
2026-07-03 19:52:41 +0300

    Update typescript, eslint and depended packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,46 +1,49 @@
-import eslint from '@eslint/js';
-import prettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
 import vitest from '@vitest/eslint-plugin';
-import tseslint, { configs, plugin } from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  configs.recommendedTypeChecked,
-  configs.strictTypeChecked,
-  configs.stylisticTypeChecked,
-  vitest.configs.recommended,
-  {
-    ignores: ['**/node_modules/**', '**/dist/**'],
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        // projectService: true,
-        projectService: {
-          allowDefaultProject: ['*.cjs', '*.mjs'],
+export default defineConfig({
+  files: ['**/*.{js,ts}'],
+  ignores: ['**/node_modules/**', '**/dist/**'],
+
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    vitest.configs.recommended,
+    {
+      languageOptions: {
+        parserOptions: {
+          // projectService: true,
+          projectService: {
+            allowDefaultProject: ['*.cjs', '*.mjs'],
+          },
+          tsconfigRootDir: import.meta.dirname,
         },
-        tsconfigRootDir: import.meta.dirname,
+      },
+      plugins: {
+        '@typescript-eslint': tseslint.plugin,
+        'simple-import-sort': simpleImportSort,
+        vitest,
+      },
+      rules: {
+        '@typescript-eslint/no-extraneous-class': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        'simple-import-sort/imports': [
+          'warn',
+          {
+            groups: [['^node:', '^@?\\w', '^\\u0000'], ['^(@hdotu)'], ['^\\.']],
+          },
+        ],
+        'simple-import-sort/exports': 'warn',
       },
     },
-    plugins: {
-      '@typescript-eslint': plugin,
-      'simple-import-sort': simpleImportSort,
-      vitest,
-    },
-    rules: {
-      '@typescript-eslint/no-extraneous-class': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      'simple-import-sort/imports': [
-        'warn',
-        {
-          groups: [['^node:', '^@?\\w', '^\\u0000'], ['^(@hdotu)'], ['^\\.']],
-        },
-      ],
-      'simple-import-sort/exports': 'warn',
-    },
-  },
-  prettier,
-);
+    prettier,
+  ],
+});
